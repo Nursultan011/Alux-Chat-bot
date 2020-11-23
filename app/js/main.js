b28d59737df14a048a9f5dc9d8b3372033215150
Nursultan <nmukhamedali@gmail.com>
2020-11-23 17:55:10 +0600

    NEW

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
-$(function () {
+$(document).ready($(function () {
+
+    // ------ Модальное окно -----
+
+    $('.popup-open').click(function () {
+        $('.popup-fade').fadeIn();
+        return false;
+    });
+
+    $('.popup-close').click(function () {
+        $(this).parents('.popup-fade').fadeOut();
+        return false;
+    });
+
+    $('.popup-fade').click(function (e) {
+        if ($(e.target).closest('.popup').length == 0) {
+            $(this).fadeOut();
+        }
+    });
+
+    // ------ Бургер меню ----------
 
     $('.hamburger').on('click', function (e) {
         e.preventDefault;
@@ -20,11 +40,15 @@ $(function () {
         $('.active__menu').fadeOut();
     });
 
+    // ----- Слайдер -------
 
     $('.work-us__sliders').slick({
         prevArrow: '<button class="slick-prev"><img src="../img/main-page/arrow-left.svg"></button>',
         nextArrow: '<button class="slick-next"><img src="../img/main-page/arrow-right.svg"></button>'
     });
+
+    // ------ Анимация с цифрами -------
+
     let scrolled = false
     let pageOffset = 400
     function myFunction(x) {
@@ -59,4 +83,17 @@ $(function () {
         setCount(worker, 'worker', 30, 25)
         setCount(project, 'project', 1, 500)
     }
-});
+
+    // ----- Табы ------
+
+    let qur = 0
+
+    $(".tabs").first().show();
+    $(".news-content__btn")[qur].classList.add('news-content__btn-active')
+    $(".news-content__btn").click(function () {
+        $(".news-content__btn")[qur].classList.remove('news-content__btn-active')
+        $(this).addClass('news-content__btn-active')
+        qur = $(".news-content__btn").index(this)
+        $(".tabs").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass(".news-content__btn-active");
+}));
